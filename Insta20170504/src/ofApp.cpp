4b86047f7e85f67160ef5e64647563b5595ac3cc
junkiyoshi <junkiyoshi@gmail.com>
2017-05-04 16:03:39 +0900

    First

---
+++ Insta20170504/src/ofApp.cpp
@@ -0,0 +1,103 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetFrameRate(60);
+	ofBackground(0);
+	ofEnableDepthTest();
+
+	ofSetWindowTitle("noise");
+	
+	ofSetLineWidth(3);
+	this->noise_z = ofRandom(10);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	cam.begin();
+
+	float radius = 30;
+	float tmp_noise_z = this->noise_z;
+	ofVec3f prePosition = ofVec3f(0, 0, 0);
+
+	float x, y, z;
+	for (int i = 360 * 5; i > 0; i--) {
+		x = radius * cos(DEG_TO_RAD * i);
+		y = radius * sin(DEG_TO_RAD * i);
+		z = ofMap(ofNoise(tmp_noise_z), 0, 1, -100, 100);
+
+		ofColor c;
+		c.setHsb(i % 255, 255, 255);
+		ofSetColor(c);
+		ofLine(prePosition, ofVec3f(x, y, z));
+		//ofEllipse(x, y, z, 10, 10);
+
+		prePosition = ofVec3f(x, y, z);
+		radius += 0.25;
+		tmp_noise_z += 0.05;
+	}
+	
+	this->noise_z += 0.05;
+
+	cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
